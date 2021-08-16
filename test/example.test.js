bafddd349309ca82f69a5b82f9fb126158900984
yosefkirkcohen <ycohen45@yahoo.com>
2021-08-16 16:03:15 -0700

    working test

--- test/example.test.js
+++ test/example.test.js
@@ -1,6 +1,8 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { countsAsAYes } from "../utils.js";
+
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
@@ -11,6 +13,48 @@ test('time to test a function', (expect) => {
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = true;
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('yes test 1', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = true;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = countsAsAYes('Yeaaaaaaaah');
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('yes test 2', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = true;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = countsAsAYes('yaaas');
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('yes test 3', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = false;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = countsAsAYes('Naaawayman');
 
     //Expect
     // Make assertions about what is expected versus the actual result
